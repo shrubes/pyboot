a594370f967b10334e9d8263eccccca741739dde
shrubes <scotthrubes@gmail.com>
2021-01-24 22:04:15 -0600

    more files from class

---
+++ booleans_and_conditionals/basic_if_else.py
@@ -0,0 +1,25 @@
+# NO TOUCHING PLEASE---------------
+from random import randint
+
+choice = randint(1, 10)
+# NO TOUCHING PLEASE---------------
+
+# YOUR CODE GOES HERE:
+if choice == 7:
+    print("lucky")
+else:
+    print("unlucky")
+
+
+# NO TOUCHING ======================================
+from random import randint
+
+num = randint(1, 1000)
+# NO TOUCHING ======================================
+
+
+# YOUR CODE GOES HERE vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+if num % 2 != 0:
+    print("odd")
+else:
+    print("even")
